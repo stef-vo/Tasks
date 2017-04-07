68f836e8fb8e09f517201046fc8fad3f3fbb6068
stef-vo <stef_vo@ukr.net>
2017-04-07 11:47:31 +0300

    Three functions merged into one

--- Task3/Header.h
+++ Task3/Header.h
@@ -2,13 +2,29 @@
 
 #include <boost/filesystem.hpp>
 #include <thread>
-#include <chrono>
+#include <queue>
+#include <mutex>
 
-// Counts files 
-int CountFiles(std::string dir, std::vector<std::string> ext_vector, std::vector <std::string> & file_names);
+/* Creates queue of files Tasks with extensions ext_vector in directory dir. */
+void CreateQueue(std::string dir, std::vector<std::string> ext_vector,
+					  std::queue <std::string> & Tasks, unsigned & total_files);
 
 /* Checks if str is blank. */
-bool IsBlank(const std::string str);
+bool IsBlank(const std::string & str);
 
-/* Counts lines in file_str. */
-void CountLines(std::string file_str, int & blank_lines, int & comment_lines, int & code_lines, int & all_lines);
+/* Counts lines in file. */
+void CountLines(std::fstream & file, unsigned & lines, unsigned & code_lines,
+	unsigned & blank_lines, unsigned & comment_lines);
+
+
+/* Gets files from the queue. */
+void GetResult(std::queue <std::string> & Tasks, unsigned & total_lines,
+	unsigned & total_code_lines, unsigned & total_blank_lines,
+	unsigned & total_comment_lines);
+
+
+// Prints files info
+void PrintResult(unsigned & total_files, unsigned & total_lines, unsigned & total_code_lines,
+	unsigned & total_blank_lines, unsigned & total_comment_lines);
+
+
--- Task3/Task3Main.cpp
+++ Task3/Task3Main.cpp
@@ -3,7 +3,7 @@
 using namespace std::chrono;
 
 int main()
-{   
+{
 	high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
 	// Create vector of extensions
@@ -14,103 +14,48 @@ int main()
 	ext_vector.push_back(".h");
 	ext_vector.push_back(".hpp");
 
-	std::string dir("E:\\Boost\\boost_1_52_0"); // path to the root directory may be changed if you like
-	std::cout << "Looking for files with extensions";
+	std::cout << "The task is to print info about all the files\n";
+	std::cout << "with the extensions ";
 	for (it = ext_vector.begin(); it<ext_vector.end(); it++)
 		std::cout << ' ' << *it;
-	std::cout << " in directory " << dir << "." << "\n\n";
+	//std::cout << "\n";
 
-	std::vector <std::string> file_names; // vector of file names with given extensions
-	std::cout << "Files in " << dir << " with these extensions found: ";
-	int total_num_files = CountFiles(dir, ext_vector, file_names);
-	std::cout << total_num_files;
-	std::cout << "\n\n";
-
-	//std::string file_name("E:\\Root\\file.cpp"); // display info about one file
+    std::string dir("E:\\Boost\\boost_1_52_0\\boost\\asio"); // path to the root directory may be changed as you wish
+	std::queue <std::string> file_names; // vector of file names with given extensions
+	std::cout << "\n" << "in directory " << dir << "\n\n";
+	std::cout << "Wait a little!" << "\n\n";
+	unsigned num_files;
+	CreateQueue(dir, ext_vector, file_names, num_files);
 	
-	int blank_lines;
-	int comment_lines;
-	int code_lines;
-	int all_lines;
-		
-	const int num_threads = 4; // number of threads (may be changed as you wish)
-	int num_cycles = total_num_files % num_threads; // so we have num_threads threads in every cycle
+	unsigned total_lines;
+	unsigned total_code_lines;
+	unsigned total_blank_lines;
+	unsigned total_comment_lines;
 
-	// announce and initialize variables for all the files in root directory
-	int total_lines = 0;
-	int total_code_lines = 0;
-	int total_blank_lines = 0;
-	int total_comment_lines = 0;
+	GetResult(file_names, total_lines, total_code_lines, total_blank_lines, total_comment_lines);
 
-	// creating threads
-	for (int i = 0; i < num_cycles; ++i)
-	{
+	PrintResult(num_files, total_lines, total_code_lines, total_blank_lines, total_comment_lines);
+	
+	unsigned num_threads = std::thread::hardware_concurrency();
+
 		std::thread *tt = new std::thread[num_threads];
-		for (int j = 0; j < num_threads; ++j)
+		for (unsigned j = 0; j < num_threads; ++j)
 		{
-			tt[j] = std::thread(CountLines, file_names[num_threads * i + j],
-				std::ref(blank_lines), std::ref(comment_lines), 
-				std::ref(code_lines), std::ref(all_lines));
-
-			total_lines += all_lines;
-			total_code_lines += code_lines;
-			total_blank_lines += blank_lines;
-			total_comment_lines += comment_lines;
+			tt[j] = std::thread(GetResult, std::ref(file_names), std::ref(total_lines),
+				std::ref(total_code_lines), std::ref(total_blank_lines),
+				std::ref(total_comment_lines));
 		}
 
-		for (int j = 0; j < num_threads; ++j)
+		for (unsigned j = 0; j < num_threads; ++j)
 			tt[j].join();
 		
 		delete[] tt;
-	}
-
-	// still (total_num_files - num_cycles * num_threads) files left
-	for (int j = num_cycles * num_threads; j < total_num_files; j++)
-	{
-		CountLines(file_names[j], blank_lines, comment_lines, code_lines, all_lines);
-		
-		total_lines += all_lines;
-		total_code_lines += code_lines;
-		total_blank_lines += blank_lines;
-		total_comment_lines += comment_lines;
-	}
-			
-	// display output
-	std::cout << "Display information on root directory " << dir << "\n\n";
-	std::cout << "Total files:           " << total_num_files << "\n";
-	std::cout << "Total blank lines:    " << total_blank_lines << "\n";
-	std::cout << "Total comment lines:  " << total_comment_lines << "\n";
-	std::cout << "Total code lines:    " << total_code_lines << "\n";
-	std::cout << "Total lines:         " << total_lines << "\n\n";
-		
-	std::cout << "It took ";
+	
+	std::cout << "\nIt took the program ";
 	high_resolution_clock::time_point t2 = high_resolution_clock::now();
 	auto duration = duration_cast<milliseconds>( t2 - t1 ).count();
-	std::cout << duration << " milliseconds to run this program with " << num_threads << " threads";
+	std::cout << duration << " milliseconds to run with " << num_threads << " threads";
 	std::cout << "\n\n";
 
-	// output to the file E:\\info.txt; you can change file path if you like
-	std::ofstream output_file;
-	output_file.open("E:\\info.txt");
-	if (output_file.is_open())
-	{
-		output_file << "Root directory " << dir << "\n\n";
-		output_file << "Total files:           " << total_num_files << "\n";
-		output_file << "Total blank lines:    " << total_blank_lines << "\n";
-		output_file << "Total comment lines:  " << total_comment_lines << "\n";
-		output_file << "Total code lines:    " << total_code_lines << "\n";
-		output_file << "Total lines:         " << total_lines << "\n\n";
-		output_file << "It took " << duration / 1000 <<
-			" seconds to run this program with " << num_threads << " threads"; 
-		output_file.close();
-	}
-	else
-	{
-		std::cout << "Impossible to open output file.";
-		return 1;
-	}
-	
-	// it's all folks
 	return 0;
 }
-
